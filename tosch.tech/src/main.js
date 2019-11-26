ec01b87bc5a322305118c9af384b790311576b99
thomas.tosch <thomas.tosch@uha.fr>
2019-11-26 22:12:59 +0100

    Added Vue project

---
+++ tosch.tech/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap';
+import 'pikaday';
+import 'animate.css/animate.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'material-icons/css/material-icons.min.css';
+import 'lato-font/css/lato-font.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'ionicons/dist/css/ionicons.min.css';
+
+Vue.config.productionTip = false;
+
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
